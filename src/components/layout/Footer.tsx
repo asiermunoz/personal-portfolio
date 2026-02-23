0bab01a545da9d4c3fde648beea175801d4acd90
asiermunoz <asiermunozlandi@gmail.com>
2026-02-22 21:27:15 -0400

    pending list

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,10 @@
 import '/Users/asiermunoz/Documents/personal-portfolio/src/styles/Footer.css'
 
+//PENDING: Add links for social media
 function Footer() {
     return (
         <footer className="footer" id="contact">
-            <span> &copy; 2025 Asier Munoz. Todos los derechos reservados</span>
+            <span> &copy; 2026 Asier Munoz. Todos los derechos reservados</span>
         </footer>
     )
 }
